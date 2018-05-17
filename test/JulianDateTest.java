d2639944c7a024c41f6540de1913c2f3d6a5539f
Luis Hankel <kaes3kuch3n@gmail.com>
2018-05-17 10:28:24 +0200

    Added test class

---
+++ test/JulianDateTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JulianDateTest {
+
+    private JulianDate date;
+
+    @Before
+    public void setUp() {
+        date = new JulianDate(2018, 05, 16);
+    }
+
+    @Test
+    public void tomorrow() {
+        assertEquals(date.tomorrow() - date.getJulianDate(), 1);
+    }
+
+    @Test
+    public void yesterday() {
+        assertEquals(date.getJulianDate() - date.yesterday(), 1);
+    }
+
+    @Test
+    public void daysBetween() {
+        assertEquals(date.daysBetween(new JulianDate(2018, 05, 10)), 6);
+    }
+
+    @Test
+    public void weekday() {
+        assertEquals(date.weekday(), Weekday.WEDNESDAY);
+    }
+
+    @Test
+    public void toGregorianDate() {
+        assertEquals(date.toGregorianDate(), "16.05.2018");
+    }
+
+    @Test
+    public void toMetricDate() {
+        assertEquals(date.toMetricDate(), "Day 5, Week 5, Month 2, Year 2458");
+    }
+}
