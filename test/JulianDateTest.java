53d3fe248cbb5edbc3f224a6d2dc3c651031d513
Luis Hankel <kaes3kuch3n@gmail.com>
2018-05-20 19:25:46 +0200

    Fixed formatting for better readability

--- test/JulianDateTest.java
+++ test/JulianDateTest.java
@@ -13,38 +13,48 @@ public class JulianDateTest {
     }
 
     @Test
-    public void tomorrow() { assertEquals(1,date.tomorrow() - date.getJulianDate());
+    public void tomorrow() {
+        assertEquals(1,date.tomorrow() - date.getJulianDate());
     }
 
     @Test
-    public void yesterday() { assertEquals(1, date.getJulianDate() - date.yesterday());
+    public void yesterday() {
+        assertEquals(1, date.getJulianDate() - date.yesterday());
     }
 
     @Test
-    public void daysBetween() { assertEquals(6, date.daysBetween(new JulianDate(2018, 5, 10)));
+    public void daysBetween() {
+        assertEquals(6, date.daysBetween(new JulianDate(2018, 5, 10)));
     }
 
     @Test
-    public void daysBetweenReverse() { assertEquals(6, date.daysBetween(new JulianDate(2018, 5, 22)));
+    public void daysBetweenReverse() {
+        assertEquals(6, date.daysBetween(new JulianDate(2018, 5, 22)));
     }
 
     @Test
-    public void weekday() { assertEquals(Weekday.WEDNESDAY, date.weekday());
+    public void weekday() {
+        assertEquals(Weekday.WEDNESDAY, date.weekday());
     }
 
     @Test
-    public void toGregorianDate() { assertEquals("16.05.2018", date.toGregorianDate());
+    public void toGregorianDate() {
+        assertEquals("16.05.2018", date.toGregorianDate());
     }
 
     @Test
-    public void toMetricDate() { assertEquals("Day 5, Week 5, Month 2, Year 2458", date.toMetricDate());
+    public void toMetricDate() {
+        assertEquals("Day 5, Week 5, Month 2, Year 2458", date.toMetricDate());
     }
 
     @Test
-    public void getJulianDate() { assertEquals(2458255, date.getJulianDate());
+    public void getJulianDate() {
+        assertEquals(2458255, date.getJulianDate());
     }
 
     @Test
-    public void setJulianDate() { date.setJulianDate(42); assertEquals(date.getJulianDate(), 42);
+    public void setJulianDate() {
+        date.setJulianDate(42);
+        assertEquals(42, date.getJulianDate());
     }
 }
